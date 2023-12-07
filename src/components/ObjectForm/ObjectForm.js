bc0abc2c08ada56309036d1b18ea18d7cadbe9c5
danila isaev <pco@volga-shield.com>
2023-12-07 16:49:07 +0400

    form whitout file input

--- src/components/ObjectForm/ObjectForm.js
+++ src/components/ObjectForm/ObjectForm.js
@@ -1,29 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./ObjectForm.module.css";
 import InputField from "./InputField";
 
 const address = [
   { label: "Улица", type: "text", name: "street" },
   { label: "д.", type: "text", name: "house" },
-  { label: "этаж", type: "number", name: "floor" },
-  { label: "этажное здание", type: "number", name: "maxFloor" },
-  { label: "жилого", type: "checkbox", name: "residential" },
-  { label: "административного", type: "checkbox", name: "administrative" },
-  { label: "стоящего отдельно", type: "checkbox", name: "standAlone" },
-  { label: "материал стен здания", type: "text", name: "walls" },
-  { label: "цвет", type: "text", name: "color" },
+  { label: "Этаж", type: "number", name: "floor" },
+  { label: "Сколько этажей?", type: "number", name: "maxFloor" },
+  { label: "Материал стен здания", type: "text", name: "walls" },
+  { label: "Цвет стен", type: "text", name: "color" },
   {
-    label: "на пересечении примерно",
+    label: "На пересечении, примерно в (м)",
     type: "number",
     name: "lenghtFromCrossroad",
   },
-  { label: "м. перекрестка ул.", type: "text", name: "crossroad" },
+  { label: "От перекрестка ул.", type: "text", name: "crossroad" },
   { label: "Вход", type: "text", name: "entry" },
   { label: "Подъезд с улицы", type: "text", name: "entrance" },
   {
     label: "Ближайшие ориентиры (рядом находящиеся магазины, вывески и т.д.)",
     type: "text",
     name: "landmark",
+  },
+];
+
+const checkboxes = [
+  {
+    label: "Жилой?",
+    type: "checkbox",
+    name: "residential",
+  },
+  {
+    label: "Административное здание?",
+    type: "checkbox",
+    name: "administrative",
+  },
+  {
+    label: "Стоит отдельно?",
+    type: "checkbox",
+    name: "standAlone",
   },
 ];
 
@@ -38,29 +53,51 @@ const equipment = [
 ];
 
 const facade = [
-  { label: "отделан", type: "text", name: "decoration" },
-  { label: "вывеска", type: "text", name: "sign" },
-  { label: "выходит", type: "text", name: "goes" },
-  { label: "на окнах", type: "text", name: "onWindos" },
-  { label: "козырек", type: "text", name: "visor" },
+  { label: "Отделан (чем? цвет)", type: "text", name: "decoration" },
+  { label: "Вывеска (что написано? цвет)", type: "text", name: "sign" },
+  { label: "Выходит (сколько дверей, окон? цвет)", type: "text", name: "goes" },
+  {
+    label: "На окнах (решетки, рольставни, цвет?)",
+    type: "text",
+    name: "onWindos",
+  },
+  {
+    label: "Козырек (есть? из чего сделан? цвет)",
+    type: "text",
+    name: "visor",
+  },
 ];
 
 const back = [
-  { label: "выходит", type: "text", name: "backExit" },
-  { label: "Как осмотреть?", type: "text", name: "inspecting" },
+  {
+    label: "Выходит (сколько дверей, окон? решетки, цвет?)",
+    type: "text",
+    name: "backExit",
+  },
+  {
+    label: "Как осмотреть?",
+    type: "text",
+    name: "inspecting",
+  },
 ];
 
-const sides = [{ label: "выходит", type: "text", name: "sideExit" }];
+const sides = [
+  {
+    label: "Выходит (сколько дверей, окон? цвет)",
+    type: "text",
+    name: "sideExit",
+  },
+];
 
 const video = [
   {
-    label: "Количество внутренних",
+    label: "Кол-во внутренних",
     type: "number",
     name: "numberOfVideoInside",
   },
-  { label: "внешних", type: "number", name: "numberOfVideoOutside" },
+  { label: "Кол-во внешних", type: "number", name: "numberOfVideoOutside" },
   { label: "Емкость диска", type: "text", name: "diskCapacity" },
-  { label: "удаленный доступ", type: "text", name: "remoteControl" },
+  { label: "Удаленный доступ", type: "text", name: "remoteControl" },
   { label: "Видеорег. канал", type: "text", name: "videoChannal" },
 ];
 
@@ -81,7 +118,7 @@ const acceptance = [
   { label: "Принял инженер", type: "text", name: "engineer" },
   { label: "Дог. на обсл. ПС", type: "text", name: "FA" },
   {
-    label: "Монтировал (если сторонняя организация",
+    label: "Монтировал (если сторонняя организация)",
     type: "text",
     name: "mounted",
   },
@@ -94,14 +131,14 @@ const acceptance = [
 ];
 
 const ObjectForm = () => {
-  const [formData, setFormData] = useState({
+  const initialFormData = {
     street: "",
     house: "",
     floor: "",
     maxFloor: "",
-    residential: "",
-    administrative: "",
-    standAlone: "",
+    residential: false,
+    administrative: false,
+    standAlone: false,
     walls: "",
     color: "",
     lenghtFromCrossroad: "",
@@ -150,147 +187,227 @@ const ObjectForm = () => {
     agreed: "",
     sticker: "",
     stickerNumber: "",
+  };
+
+  const [file, setfile] = useState(null);
+  const [formData, setFormData] = useState(() => {
+    const savedData = localStorage.getItem("formData");
+    const initialData = savedData ? JSON.parse(savedData) : initialFormData;
+
+    // Для чекбоксов при обновлении выставляются правильные значения булевого типа
+    const updatedData = Object.entries(initialData).reduce(
+      (acc, [key, value]) => {
+        if (checkboxes.some((checkbox) => checkbox.name === key)) {
+          acc[key] = value === "true";
+        } else {
+          acc[key] = value;
+        }
+        return acc;
+      },
+      {}
+    );
+    return updatedData;
   });
 
+  // useEffect для сохранения значений в localStorage
+  useEffect(() => {
+    localStorage.setItem("formData", JSON.stringify(formData));
+  }, [formData]);
+
   const handleInputChange = (fieldName) => (fieldValue) => {
-    setFormData({ ...formData, [fieldName]: fieldValue });
+    if (fieldName === "scheme") {
+      setfile(fieldValue);
+    } else {
+      setFormData({ ...formData, [fieldName]: fieldValue });
+    }
+  };
+
+  const objectToFormData = (obj) => {
+    const formData = new FormData();
+    Object.entries(obj).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+    return formData;
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData);
-    alert(JSON.stringify(formData));
+    const data = objectToFormData(formData);
+
+    if (file) {
+      data.append("shceme", file);
+    }
+    data.forEach((value, key) => {
+      console.log(`${key}: ${value}`);
+    });
+    alert(JSON.stringify(data));
   };
 
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
-      <div className={styles.objectNumber}>
+      <fieldset>
+        <div className={styles.objectNumber}>
+          <InputField
+            label='Пультовый номер'
+            type='number'
+            value={formData.objectNumber}
+            onChange={handleInputChange("objectNumber")}
+          />
+        </div>
         <InputField
-          label='Пультовый номер'
+          label='Наименование'
+          type='text'
+          value={formData.name}
+          onChange={handleInputChange("name")}
+        />
+      </fieldset>
+      <h4>Адрес</h4>
+      <fieldset>
+        {address.map(({ label, type, name }, index) => (
+          <InputField
+            key={index}
+            label={label}
+            type={type}
+            value={formData[name]}
+            onChange={handleInputChange(name)}
+          />
+        ))}
+        {checkboxes.map(({ label, type, name }, index) => (
+          <InputField
+            key={index}
+            label={label}
+            type={type}
+            value={formData[name]}
+            onChange={() => handleInputChange(name)(!formData[name])}
+          />
+        ))}
+      </fieldset>
+      <h4>Оборудование</h4>
+      <fieldset>
+        {equipment.map(({ label, type, name }, index) => (
+          <InputField
+            key={index}
+            label={label}
+            type={type}
+            value={formData[name]}
+            onChange={handleInputChange(name)}
+          />
+        ))}
+      </fieldset>
+      <h4>Фасад</h4>
+      <fieldset>
+        {facade.map(({ label, type, name }, index) => (
+          <InputField
+            key={index}
+            label={label}
+            type={type}
+            value={formData[name]}
+            onChange={handleInputChange(name)}
+          />
+        ))}
+      </fieldset>
+      <h4>Тыл</h4>
+      <fieldset>
+        {back.map(({ label, type, name }, index) => (
+          <InputField
+            key={index}
+            label={label}
+            type={type}
+            value={formData[name]}
+            onChange={handleInputChange(name)}
+          />
+        ))}
+      </fieldset>
+      <h4>Торец (правый, левый)</h4>
+      <fieldset>
+        {sides.map(({ label, type, name }, index) => (
+          <InputField
+            key={index}
+            label={label}
+            type={type}
+            value={formData[name]}
+            onChange={handleInputChange(name)}
+          />
+        ))}
+      </fieldset>
+      <h4>Примечание</h4>
+      <fieldset>
+        <InputField
+          label=''
+          type='text'
+          value={formData.note}
+          onChange={handleInputChange("note")}
+        />
+        <p>
+          Указать уязвимые места, пути обхода, люки, шахты, подвалы, также пути
+          отхода, расположение блока и т.д
+        </p>
+      </fieldset>
+      <h4>Сведения по видеонаблюдению</h4>
+      <fieldset>
+        {video.map(({ label, type, name }, index) => (
+          <InputField
+            key={index}
+            label={label}
+            type={type}
+            value={formData[name]}
+            onChange={handleInputChange(name)}
+          />
+        ))}
+      </fieldset>
+      <h4>Схема объекта с расположением датчикова</h4>
+      <fieldset>
+        <InputField
+          label=''
+          type='file'
+          onChange={(file) => handleInputChange("scheme")(file)}
+        />
+        <InputField
+          label='План расстановки оборудования согласован'
+          type='text'
+          value={formData.agreed}
+          onChange={handleInputChange("agreed")}
+        />
+      </fieldset>
+      <h4>Описание шлейфов</h4>
+      <fieldset>
+        {loops.map(({ label, type, name }, index) => (
+          <InputField
+            key={index}
+            label={label}
+            type={type}
+            value={formData[name]}
+            onChange={handleInputChange(name)}
+          />
+        ))}
+
+        <InputField
+          label='Наклеен стикер'
+          type='text'
+          value={formData.sticker}
+          onChange={handleInputChange("sticker")}
+        />
+        <InputField
+          label='Кол-во'
           type='number'
-          value={formData.objectNumber}
-          onChange={handleInputChange("objectNumber")}
+          value={formData.stickerNumber}
+          onChange={handleInputChange("stickerNumber")}
         />
-      </div>
-      <InputField
-        label='Наименование'
-        type='text'
-        value={formData.name}
-        onChange={handleInputChange("name")}
-      />
-      <h1>Адрес</h1>
-      {address.map(({ label, type, name }, index) => (
-        <InputField
-          key={index}
-          label={label}
-          type={type}
-          value={formData[name]}
-          onChange={handleInputChange(name)}
-        />
-      ))}
-      <h1>Оборудование</h1>
-      {equipment.map(({ label, type, name }, index) => (
-        <InputField
-          key={index}
-          label={label}
-          type={type}
-          value={formData[name]}
-          onChange={handleInputChange(name)}
-        />
-      ))}
-      <h1>Фасад</h1>
-      {facade.map(({ label, type, name }, index) => (
-        <InputField
-          key={index}
-          label={label}
-          type={type}
-          value={formData[name]}
-          onChange={handleInputChange(name)}
-        />
-      ))}
-      <h1>Тыл</h1>
-      {back.map(({ label, type, name }, index) => (
-        <InputField
-          key={index}
-          label={label}
-          type={type}
-          value={formData[name]}
-          onChange={handleInputChange(name)}
-        />
-      ))}
-      <h1>Торец (правый, левый)</h1>
-      {sides.map(({ label, type, name }, index) => (
-        <InputField
-          key={index}
-          label={label}
-          type={type}
-          value={formData[name]}
-          onChange={handleInputChange(name)}
-        />
-      ))}
-      <h1>Примечание</h1>
-      <InputField
-        label=''
-        type='text'
-        value={formData.note}
-        onChange={handleInputChange("note")}
-      />
-      <p>Указать уязвимые места, пути обхода, расположение блока и т.д</p>
-      <h1>Сведения по видеонаблюдению</h1>
-      {video.map(({ label, type, name }, index) => (
-        <InputField
-          key={index}
-          label={label}
-          type={type}
-          value={formData[name]}
-          onChange={handleInputChange(name)}
-        />
-      ))}
-      <h1>Схема объекта с расположением датчикова</h1>
-      <InputField
-        label=''
-        type='file'
-        value={formData.scheme}
-        onChange={handleInputChange("scheme")}
-      />
-      <InputField
-        label='План расстановки оборудования согласован'
-        type='text'
-        value={formData.agreed}
-        onChange={handleInputChange("agreed")}
-      />
-      {loops.map(({ label, type, name }, index) => (
-        <InputField
-          key={index}
-          label={label}
-          type={type}
-          value={formData[name]}
-          onChange={handleInputChange(name)}
-        />
-      ))}
-      <InputField
-        label='Наклеен стикер'
-        type='text'
-        value={formData.sticker}
-        onChange={handleInputChange("sticker")}
-      />
-      <InputField
-        label='Кол-во'
-        type='number'
-        value={formData.stickerNumber}
-        onChange={handleInputChange("stickerNumber")}
-      />
-      {acceptance.map(({ label, type, name }, index) => (
-        <InputField
-          key={index}
-          label={label}
-          type={type}
-          value={formData[name]}
-          onChange={handleInputChange(name)}
-        />
-      ))}
+      </fieldset>
+      <h4>Описание монтажа, приемка оборудования</h4>
+      <fieldset>
+        {acceptance.map(({ label, type, name }, index) => (
+          <InputField
+            key={index}
+            label={label}
+            type={type}
+            value={formData[name]}
+            onChange={handleInputChange(name)}
+          />
+        ))}
+      </fieldset>
       <button type='submit' className={styles.button}>
-        Submit
+        Отправить
       </button>
     </form>
   );
